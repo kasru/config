a313fcb0479797952b2d37562999328511658fee
Aleksandr.Kiselev <Aleksandr.Kiselev@infotecs.ru>
2018-03-29 18:44:00 +0300

    Add qstring.py for lldb

---
+++ .lldb/qstring.py
@@ -0,0 +1,24 @@
+import lldb
+
+#(lldb) command script import ~/.lldb/qstring.py
+
+def utf16string_summary(value, *rest):
+    d = value.GetChildMemberWithName("d")
+    length = d.GetChildMemberWithName("size").GetValueAsSigned()
+    offset = d.GetChildMemberWithName("offset").GetValueAsSigned()
+    address = d.GetValueAsUnsigned() + offset
+
+    if length == 0:
+        return '""'
+    error = lldb.SBError()
+    # UTF-16, so we multiply length by 2
+    bytes = value.GetProcess().ReadMemory(address, length * 2, error)
+    if bytes is None:
+        return '""'
+    return '"%s"' % (bytes.decode('utf-16').encode('utf-8'))
+
+def __lldb_init_module(debugger, *rest):
+    print "registering QString"
+    summary = lldb.SBTypeSummary.CreateWithFunctionName("qstring.utf16string_summary")
+    summary.SetOptions(lldb.eTypeOptionHideChildren)
+    debugger.GetDefaultCategory().AddTypeSummary( lldb.SBTypeNameSpecifier("QString", False), summary )
